08632608e8fb1a626d64ce679539339e4bc2ee0d
Dian Chen <okcd00@qq.com>
2022-04-01 18:16:50 +0800

    Update: client with auto re-connection.

---
+++ Triggernometry/parsing_client.py
@@ -0,0 +1,66 @@
+# -*- coding: UTF-8 -*-
+# =====================================================
+#   Copyright (C) since 2022 All rights reserved.
+#
+#   filename : parsing_server.py
+#   author   : okcd00 / okcd00@qq.com
+#   date     : 2022-03-31
+#   desc     : A simple socket server and client 
+#              for interaction between different processes.
+# =====================================================
+import json
+import time
+import socket
+
+
+class ParsingClient(object):
+    def __init__(self, port=2023):
+        self.clientsocket = None
+        self.port = port
+        self.renew_client(port)
+
+    def renew_client(self, port=None):
+        port = port or self.port
+        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientsocket.connect(('localhost', port))
+
+    def sent_data(self, text=None):
+        """
+        发送数据,短连接
+        :param data: 要发送的数据 [str]
+        :return: 收到的服务端发来的数据或状态 [str]
+        """
+        if text:
+            self.clientsocket.sendall(text.encode('utf-8'))
+            recv = self.clientsocket.recv(1024)
+            self.clientsocket.close()
+            return recv
+        else:
+            print("The sent data is empty or not sent!")
+
+    def sent_data_pending_input(self, exit_msg="<CLIENT_ENDS>"):
+        """
+        发送数据，长连接
+        """
+        lastdata = ''
+        while lastdata != "quit":
+            lastdata = input("请输入要发送的数据：")
+            if len(lastdata) > 0:
+                try:
+                    self.clientsocket.sendall(lastdata.encode("utf-8"))
+                    recv = self.clientsocket.settimeout(10)
+                    recv = self.clientsocket.recv(64)
+                    print(recv.decode("utf-8"))
+                except BrokenPipeError:
+                    self.renew_client()
+                    print("Renew Connection")
+            else:
+                print("The sent data is empty or not sent!")
+        self.clientsocket.sendall(exit_msg.encode("utf-8"))
+        print("Client Closed.")
+        self.clientsocket.close()
+
+
+if __name__ == '__main__':
+    ps = ParsingClient()
+    ps.sent_data_pending_input()
--- Triggernometry/parsing_server.py
+++ Triggernometry/parsing_server.py
@@ -14,11 +14,13 @@ import socket
 
 
 class ParsingServer(object):
+    BUFSIZE = 1024
     PONAZU_PORT = 2020
 
     def __init__(self, port=2023):
         self.serversocket = None
         self.clientsocket = None
+        self._BUFSIZE = 1024
         self.new_server(port=port)
         self.new_client(port=self.PONAZU_PORT)
 
@@ -35,39 +37,28 @@ class ParsingServer(object):
         self.clientsocket.send(json_str.encode('utf-8'))
         print(self.clientsocket.recv(1024))
 
-    def start_server(self):
+    def start_server(self, exit_msg="<CLIENT_ENDS>"):
         # become a server socket, maximum 10 connections
         self.serversocket.listen(5) 
         while True:
             connection, address = self.serversocket.accept()
             sender_host, sender_port = address
             try:
-                connection.settimeout(50)
+                connection.settimeout(100)
                 while True:
-                    buf = connection.recv(1024)
+                    buf = connection.recv(64, socket.MSG_WAITALL)
                     if len(buf) > 0:
-                        connection.send("Approved buf".encode('utf-8'))
+                        # connection.send("Received".encode('utf-8'))
+                        connection.sendall(f"{buf}".encode('utf-8'))
                         text = buf.decode('utf-8')
                         print(f"[{time.ctime()}] {sender_host}:{sender_port} says \n{text}")
-                        if text == 'end':
+                        if text == exit_msg:
                             connection.close()
                             break
             except socket.timeout:
                 print("timeout")
             print(f"Closing current connection: {sender_host}:{sender_port}")
             connection.close()
-
-
-class ParsingClient(object):
-    def __init__(self, port=2023):
-        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clientsocket.connect(('localhost', port))
-    
-    def send(self, text):
-        self.clientsocket.send(text.encode('utf-8'))
-        print(self.clientsocket.recv(1024))
-        if text == 'end':
-            self.clientsocket.close()
 
 
 if __name__ == '__main__':
